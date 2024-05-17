5f7a90a0238efc41243a3e12015124929e37e6f1
kkuras <carretta2315@gmail.com>
2024-05-16 23:06:59 -0300

    coisas importantes feitas!!!

--- src/main/java/net/krtm/tutorialmod/TutorialMod.java
+++ src/main/java/net/krtm/tutorialmod/TutorialMod.java
@@ -17,7 +17,7 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
-    // Define mod id in a common place for everything to reference
+
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
