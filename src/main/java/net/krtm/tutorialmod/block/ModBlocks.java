ffa56c1bcb75e98d1e96621cf8ecf99651457c5a
kkuras <carretta2315@gmail.com>
2024-08-05 18:30:46 -0300

    advanced block(sound block)

--- src/main/java/net/krtm/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/krtm/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.krtm.tutorialmod.block;
 
 import net.krtm.tutorialmod.TutorialMod;
+import net.krtm.tutorialmod.block.custom.SoundBlock;
 import net.krtm.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -38,6 +39,8 @@ public class ModBlocks {
     public static final RegistryObject<Block> END_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
                     .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3,7)));
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
