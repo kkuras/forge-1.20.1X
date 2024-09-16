ca69ffc983a4406e7cdc4ddbe97baa625b3ffbbd
kkuras <carretta2315@gmail.com>
2024-09-15 23:14:18 -0300

    added items for suspicious sand

--- src/main/java/net/krtm/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/krtm/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
@@ -3,6 +3,7 @@ package net.krtm.tutorialmod.datagen;
 import net.krtm.tutorialmod.TutorialMod;
 import net.krtm.tutorialmod.item.ModItems;
 import net.krtm.tutorialmod.loot.AddItemModifier;
+import net.krtm.tutorialmod.loot.AddSusSandItemModifier;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
@@ -28,6 +29,13 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
 
         add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.METAL_DETECTOR.get()));
+
+
+        add("metal_detector_from_suspicious_sand", new AddSusSandItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build() }, ModItems.METAL_DETECTOR.get()));
+
+
+
 
 
     }
