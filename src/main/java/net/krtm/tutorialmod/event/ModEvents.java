b0b76a53daf0df415b2f0a739e741ced010a52b1
kkuras <carretta2315@gmail.com>
2024-09-19 17:32:59 -0300

    custom villager trades

---
+++ src/main/java/net/krtm/tutorialmod/event/ModEvents.java
@@ -0,0 +1,84 @@
+package net.krtm.tutorialmod.event;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.krtm.tutorialmod.TutorialMod;
+import net.krtm.tutorialmod.item.ModItems;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.EnchantedBookItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.event.village.WandererTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID)
+public class ModEvents {
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.FARMER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            // level 1
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    new ItemStack(ModItems.STRAWBERRY.get(), 12),
+                    10, 8, 0.2f));
+
+            // level 2
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(ModItems.CORN.get(), 6),
+                    5, 9, 0.035f));
+
+            // level 3
+            trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.GOLD_INGOT, 8),
+                    new ItemStack(ModItems.CORN_SEEDS.get(), 2),
+                    2, 12, 0.075f));
+
+
+
+        }
+
+        if(event.getType() == VillagerProfession.LIBRARIAN) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack enchantedBook = EnchantedBookItem.createForEnchantment(new EnchantmentInstance(Enchantments.THORNS,2));
+
+
+
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 32),
+                    enchantedBook,
+                    2, 8, 0.2f));
+        }
+
+    }
+
+    @SubscribeEvent
+    public static void addCustomWarderingTrades(WandererTradesEvent event) {
+
+        List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
+        List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
+
+        genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 12),
+                new ItemStack(ModItems.SAPPHIRE_BOOTS.get(), 1),
+                3, 0,2f));
+
+        rareTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 24),
+                new ItemStack(ModItems.METAL_DETECTOR.get(), 1),
+                2, 12,2f));
+
+
+    }
+
+}
