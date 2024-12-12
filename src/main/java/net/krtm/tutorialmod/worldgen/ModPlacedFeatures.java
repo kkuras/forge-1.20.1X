a85f2263b721d1ffb708b2138910a3ee364cdf37
kkuras <carretta2315@gmail.com>
2024-12-11 22:48:17 -0300

    custom tree generation

--- src/main/java/net/krtm/tutorialmod/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/krtm/tutorialmod/worldgen/ModPlacedFeatures.java
@@ -1,10 +1,13 @@
 package net.krtm.tutorialmod.worldgen;
 
 import net.krtm.tutorialmod.TutorialMod;
+import net.krtm.tutorialmod.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -21,6 +24,8 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_SAPPHIRE_ORE_PLACED_KEY = registerKey("nether_sapphire_ore_placed");
     public static final ResourceKey<PlacedFeature> END_SAPPHIRE_ORE_PLACED_KEY = registerKey("end_sapphire_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> PINE_PLACED_KEY = registerKey("pine_placed");
+
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
@@ -36,6 +41,10 @@ public class ModPlacedFeatures {
                 ModOrePlacement.commonOrePlacement(12,
                         HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
 
+        register(context, PINE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.PINE_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        ModBlocks.PINE_SAPLING.get()));
+
     }
 
 
@@ -49,11 +58,6 @@ public class ModPlacedFeatures {
                                  List<PlacementModifier> modifiers) {
         context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
     }
-
-
-
-
-
 
 
 
